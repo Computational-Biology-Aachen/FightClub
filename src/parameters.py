c38582878c41db679c4a608a94f2ec150b7fbebc
Tanvir Hassan <tanvir.hassan@rwth-aachen.de>
2024-12-19 00:53:17 +0100

    exp data analysis and optimizer

--- src/parameters.py
+++ src/parameters.py
@@ -1,8 +1,11 @@
 parameters = {
 "beta" : 0.0001,  # loss of public and private because of competition
 "gamma" : 0.0001,  # cost spent by private population
-"alpha" : 0.0002,  # benefit from cooperator to defector  
-"mu" : 0.04,  # growth rate (benefit) of public
-"eta" : 0.02,  # growth rate (benefit) of private
-"nu" : 0.00001,  # transportation cost of defector
+"alpha" : 0.0002,  # benefit from cooperator to defector
+"eta" : 0.0001,  # cost spent by public population   
+"r_c" : 0.5,  # growth rate (benefit) of public
+"r_p" : 0.2,  # growth rate (benefit) of private
+"nu" : 0.0001,  # transportation cost of defector
+"sigma_c": 0.1,  # Noise intensity for C
+"sigma_p": 0.1   # Noise intensity for P
 }
